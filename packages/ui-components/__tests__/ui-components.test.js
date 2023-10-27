dce3b273830ef74d939e996293513d613a22b76d
Sergey Hursevich <hursik94@gmail.com>
2023-10-27 16:25:05 +0200

    lerna init

---
+++ packages/ui-components/__tests__/ui-components.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const uiComponents = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(uiComponents(), 'Hello from uiComponents');
+console.info('uiComponents tests passed');
---
+++ packages/ui-components/lib/ui-components.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = uiComponents;
+
+function uiComponents() {
+  return 'Hello from uiComponents';
+}
